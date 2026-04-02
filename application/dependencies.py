603713d9a2e526600e18d5f07110e2493638534d
Colby Wirth <wirthcolby00@gmail.com>
2026-04-02 16:38:40 -0400

    added ML module to dependencies

--- application/dependencies.py
+++ application/dependencies.py
@@ -1,15 +1,30 @@
+#application/dependencies.py
 import json
 
 from core.IngestionModule.IngestionModuleInterface import IngestionModuleInterface
+
+
 from application.services.LiveStreamService import LiveStreamService
+from application.services.MLStreamService import MLStreamService
 
 
-with open("cfg.json", 'r') as cfg:
+with open("configs/udp_cfg.json", 'r') as cfg:
     udp_cfg = json.load(cfg)
 
-_ingestion_interface = IngestionModuleInterface(udp_cfg)
 
+with open("configs/ml_cfg.json", 'r') as cfg:
+    ml_cfg = json.load(cfg)
+
+_ingestion_interface = IngestionModuleInterface(udp_cfg)
 _live_stream_service = LiveStreamService(_ingestion_interface)
+
+# TODO implement this
+# _ml_module_interface = MLModuleInterface(ml_cfg)
+# _ml_stream_service = MLStreamService(_ingestion_interface, ml_module_interface=ml_module_interface)
+# _ml_stream_service.start()
+
+# on shutdown
+# _ml_stream_service.stop()
 
 def get_live_stream_service() -> LiveStreamService:
     return _live_stream_service
