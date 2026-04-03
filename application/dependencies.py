b7c82cb2d62d5709285bb4bfefa948e8e2a3a17b
Colby Wirth <wirthcolby00@gmail.com>
2026-04-03 17:47:23 -0400

    reworked dependency manager to move start and stop logic to main.py

--- application/dependencies.py
+++ application/dependencies.py
@@ -1,40 +1,36 @@
-#application/dependencies.py
 import json
-
 from core.IngestionModule.IngestionModuleInterface import IngestionModuleInterface
 from core.MLProcessingModule.MLModuleInterface import MLModuleInterface
-
 from application.services.LiveStreamService import LiveStreamService
 from application.services.MLStreamService import MLStreamService
 from application.services.ClipIngestionService import ClipIngestionService
 
-
+# Load configs
 with open("application/configs/udp_cfg.json", 'r') as cfg:
     udp_cfg = json.load(cfg)
-
 
 with open("application/configs/ml_cfg.json", 'r') as cfg:
     ml_cfg = json.load(cfg)
 
-# --- Interfaces ---
+# Interfaces
 _ingestion_interface = IngestionModuleInterface(udp_cfg)
 _ml_module_interface = MLModuleInterface(ml_cfg)
+
 
 # Hi Sean we need the Infrastructure Interface
 # _infrastructure_module_interface = InfrastructureModuleInterface(infrastructure_cfg)
 
-
-# --- Services ---
+# Service instances
 _live_stream_service = LiveStreamService(_ingestion_interface)
 _ml_stream_service = MLStreamService(_ingestion_interface, ml_module_interface=_ml_module_interface)
 _clip_ingestion_service = ClipIngestionService(_ml_module_interface)
-_clip_ingestion_service.start()
 
 
-_ml_stream_service.start(test=True)
+# Hi Sean we also need the Auth and Notification services
+# _auth_service = UserAuthService(_infrastructure_module_interface)
+# _notification_service = NotificationService(_infrastructure_module_interface)
 
-# TODO on ctrl+c we need to gracefully call the stop functions
-#_ml_stream_service.stop()
-
-def get_live_stream_service() -> LiveStreamService:
-    return _live_stream_service
+# Accessors
+def get_live_stream_service(): return _live_stream_service
+def get_ml_stream_service(): return _ml_stream_service
+def get_clip_ingestion_service(): return _clip_ingestion_service
